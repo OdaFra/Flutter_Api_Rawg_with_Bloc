250b1b0a85512320c0d236b8a7d5b1b34c77bb5c
OdaFra <odafra@Oz.local>
2022-10-29 19:24:45 -0300

    Agregamos Bloc para Gategory en All Games

--- lib/ui/home/widgets/category_widget/logic/category_bloc.dart
+++ lib/ui/home/widgets/category_widget/logic/category_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_rawg_api/repository/game_repository.dart';
 
 import '../../../../../repository/models/genre.dart';
 
@@ -7,9 +8,29 @@ part 'category_event.dart';
 part 'category_state.dart';
 
 class CategoryBloc extends Bloc<CategoryEvent, CategoryState> {
-  CategoryBloc() : super(const CategoryState()) {
-    on<CategoryEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final GameRepository gameRepository;
+  CategoryBloc({required this.gameRepository}) : super(const CategoryState()) {
+    on<GetCategories>(getCategoriesEventToState);
+    on<SelectCategory>(selectCategoryEventToState);
+  }
+
+  void getCategoriesEventToState(
+      GetCategories event, Emitter<CategoryState> emit) async {
+    emit(state.copyWith(status: CategoryStatus.loading));
+    try {
+      final genres = await gameRepository.getGenres();
+      emit(state.copyWith(status: CategoryStatus.success, categories: genres));
+    } catch (error, stacktrace) {
+      print(stacktrace);
+      emit(state.copyWith(status: CategoryStatus.error));
+    }
+  }
+
+  void selectCategoryEventToState(
+      SelectCategory event, Emitter<CategoryState> emit) async {
+    emit(state.copyWith(
+      status: CategoryStatus.selected,
+      idSelected: event.idSelected,
+    ));
   }
 }
