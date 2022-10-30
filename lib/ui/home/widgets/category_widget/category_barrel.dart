c9f86ba097315547eb1b181ecb0cc421920f4693
Oscar Ramirez <osramirezf@gmail.com>
2022-10-30 12:19:05 -0300

    Actualizacion de los archivos de export

--- lib/ui/home/widgets/category_widget/category_barrel.dart
+++ lib/ui/home/widgets/category_widget/category_barrel.dart
@@ -1,3 +1,4 @@
 export 'logic/category_bloc.dart';
 export 'category_item.dart';
 export 'category_success_widget.dart';
+export '../category_widget/category_widget.dart';
--- lib/ui/home/widgets/games_by_category_widget/games_by_category_barrel.dart
+++ lib/ui/home/widgets/games_by_category_widget/games_by_category_barrel.dart
@@ -1,3 +1,4 @@
 export '../games_by_category_widget/games_by_category_images.dart';
 export '../games_by_category_widget/games_by_category_success_widget.dart';
 export '../games_by_category_widget/games_by_category_title.dart';
+export '../games_by_category_widget/games_by_category_widget.dart';
